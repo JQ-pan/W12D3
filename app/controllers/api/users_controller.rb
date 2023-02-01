180eb5c1ce58f95314e21b86b8640fb933398179
Jiong Qi <jiongqipan@gmail.com>
2023-02-01 14:07:48 -0500

    start frontend

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,18 +1,17 @@
 class Api::UsersController < ApplicationController
   wrap_parameters include: User.attribute_names + ['password']
 
-  before_action :require_logged_out, only: [:create]
+  # before_action :require_logged_out, only: [:create]
 
   def create
     @user = User.new(user_params)
     if @user.save
       # debugger
       login!(@user)
-      render json: @user
+      render :show
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
-    # render json: user_params
   end
 
   private
