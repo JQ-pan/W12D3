b51d53e0b32851339bd6ccac32b3a62ca0271aa7
Jiong Qi <jiongqipan@gmail.com>
2023-02-01 09:16:06 -0500

    database seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,17 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, 
-    uniqueness: true, 
-    length: { in: 3..30 }, 
-    format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+        uniqueness: true, 
+        length: { in: 3..30 }, 
+        format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
     validates :email, 
-    uniqueness: true, 
-    length: { in: 3..255 }, 
-    format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :session_token, presence: true, uniqueness: true
-    validates :password, length: { in: 6..255 }, allow_nil: true
+        uniqueness: true, 
+        length: { in: 3..255 }, 
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :session_token, 
+        presence: true, uniqueness: true
+    validates :password, 
+        length: { in: 6..255 }, allow_nil: true
 
 
     def self.find_by_credentials(credential, password) 
